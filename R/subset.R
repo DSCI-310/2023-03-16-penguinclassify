d02355ce384e0fb33c4912515989de851b541c60
Daniel Chen <chendaniely@gmail.com>
2023-04-11 09:53:53 -0700

    fixes the note for no visiable binding

--- R/subset.R
+++ R/subset.R
@@ -1,7 +1,9 @@
 #' Returns the species column for Palmerpenguins
 #' @return dataframe of 1 column from the palrmer penguins dataframe
 #' @export
+#'
+#' @importFrom rlang .data
 get_species <- function() {
   palmerpenguins::penguins %>%
-    dplyr::select(species)
+    dplyr::select(.data$species)
 }
